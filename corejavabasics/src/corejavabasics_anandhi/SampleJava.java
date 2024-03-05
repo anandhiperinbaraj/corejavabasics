b4cd11e2dc72325ca85fa62774f468f9071ddc13
PRINTER WORLD <PRINTER WORLD@DESKTOP-HD5P6M3>
2024-03-05 15:48:01 +0530

    This is my first project

---
+++ corejavabasics/src/corejavabasics_anandhi/SampleJava.java
@@ -0,0 +1,15 @@
+package corejavabasics_anandhi;
+import java.util.*;
+public class SampleJava {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner (System.in);
+		int a,b,c;
+		System.out.println("Enter the a and b values:");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Addition of a="+a+" and b="+b+" is :"+c);
+	}
+
+}
