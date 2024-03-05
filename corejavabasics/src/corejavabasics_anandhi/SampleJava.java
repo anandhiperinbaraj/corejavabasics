95ef36be05dc67d53844acd310883336d312caa8
PRINTER WORLD <PRINTER WORLD@DESKTOP-HD5P6M3>
2024-03-05 16:34:29 +0530

    also subtracted

--- corejavabasics/src/corejavabasics_anandhi/SampleJava.java
+++ corejavabasics/src/corejavabasics_anandhi/SampleJava.java
@@ -4,12 +4,14 @@ public class SampleJava {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner (System.in);
-		int a,b,c;
+		int a,b,c,d;
 		System.out.println("Enter the a and b values:");
 		a=s.nextInt();
 		b=s.nextInt();
 		c=a+b;
+		d=a-b;
 		System.out.println("Addition of a="+a+" and b="+b+" is :"+c);
+		System.out.println("subraction of a="+a+"and b="+b+"is :"+d);
 	}
 
 }
